150cf51d5e4e61db111b6c7ad9f6cd700d3b3ed6
IvailoSakaliev <ivo.sakaliev1996@gmail.com>
2018-06-11 02:33:56 +0300

    Add contact component

--- src/app/router.module.ts
+++ src/app/router.module.ts
@@ -1,19 +1,20 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from  "@angular/router";
 import { HomeComponent } from "./home/home.component";
-import {AboutComponent} from "./about/about.component"; 
+import { AboutComponent } from "./about/about.component"; 
+import { ContactsComponent } from "./contact/contacts.component";
 
 const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'about', component: AboutComponent}
-
-
+    {path: 'about', component: AboutComponent},
+    {path: 'contact', component: ContactsComponent}
 ];
 
 @NgModule({
     declarations:    [
         HomeComponent,
-        AboutComponent
+        AboutComponent,
+        ContactsComponent
     ],
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
