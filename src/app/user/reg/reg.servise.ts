6eb9f9da85bb8d789e30206f5e179b0ef4ec71fa
IvailoSakaliev <ivo.sakaliev1996@gmail.com>
2018-06-12 21:17:02 +0300

    Add functionalitu in registration AuthenticationServise

--- src/app/user/reg/reg.servise.ts
+++ src/app/user/reg/reg.servise.ts
@@ -1,7 +1,30 @@
-import {Injectable} from '@angular/core';
- 
+import * as firebase from 'firebase';
+import { Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+
 @Injectable()
 
 export class AuthemticationServise{
+    constructor(private router: Router){}
 
+    register(email: string, password: string) {
+        firebase.auth()
+        .createUserWithEmailAndPassword(email, password)
+        .catch(
+            error => console.log(error)
+        );
+    }
+
+    login(email: string, password: string){
+        firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(
+            response => {
+                sessionStorage.setItem("currentUser", response.user["email"]);
+                this.router.navigate(["/home"]);
+            }
+        )
+        .catch(
+            error => alert("Wrong credentials!")
+        );
+    }
 }
